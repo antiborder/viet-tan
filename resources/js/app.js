8dbc05123bb55bde99dde756fd162b807b47116a
antiborder <mail@antiborder.com>
2022-01-07 17:01:35 +0700

    update the new function learn

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,14 +1,12 @@
 import './bootstrap'
 import Vue from 'vue'
-//==========ここから追加==========
-import ArticleTagsInput from './components/ArticleTagsInput'
-//==========ここまで追加==========
+
+
+import Question from './components/Question'
 
 const app = new Vue({
   el: '#app',
   components: {
-    //==========ここから追加==========
-    ArticleTagsInput,
-    //==========ここまで追加==========
+    Question
   }
 })
