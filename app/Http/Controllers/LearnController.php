e4a3a36aede685b78119c7c3ac50c86fea2a270e
antiborder <mail@antiborder.com>
2022-02-01 21:23:08 +0700

    function to record easiness

--- app/Http/Controllers/LearnController.php
+++ app/Http/Controllers/LearnController.php
@@ -11,25 +11,33 @@ class LearnController extends Controller
 {
     public function store(Request $request)
     {
-
-        // $user_id = $request->user;
         $user_id = Auth::id();        
         $now = date("Y-m-d H:i:s");
         $word = Word::where('name',$request->name)->first();
-        // $word_id = $word->id;
         $word_id = $word->id;
+        switch ($request->easiness) {
+            case 0:
+                $interval_text ="now +10 min +0 hours";
+                break;
+            case 1:
+                $interval_text ="now +24 hours";
+                break;
+            case 2:
+                $interval_text ="now +72 hours";
+                break;
+            case 3:
+                $interval_text ="now +168 hours";
+                break;                
+        }
+
         $learn = Learn::create([
             'user_id'=> $user_id,
             'word_id'=> $word_id,
             'result'=> $request->result,
             'easiness' => $request->easiness,
-            'next_time'=> $now,
+            'next_time'=> date("Y-m-d H:i:s",strtotime($interval_text)),
         ]);
 
         return $word;
-
-        // return $request;
-        // return [
-        //     $learn->easiness        ];
     }
 }
