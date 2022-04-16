b048eb3ac5b1d9e4b855f7ad85a23921542496c1
antiborder <mail@antiborder.com>
2022-04-16 20:48:50 +0700

    forgot password

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -31,9 +31,9 @@
  
                 <input type="hidden" name="remember" id="remember" value="on">
 
-                <!-- <div class="text-left">
+                <div class="text-left">
                   <a href="{{ route('password.request') }}" class="card-text">パスワードを忘れた方</a>
-                </div> -->
+                </div>
 
                 <button class="btn btn-block blue-gradient mt-2 mb-2" type="submit">ログイン</button>
 
