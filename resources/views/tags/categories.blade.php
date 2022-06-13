eac8189ca2576a73830b16fbdd1449f5fb12e41e
antiborder <mail@antiborder.com>
2022-06-13 16:14:54 +0700

    put ads for test

--- resources/views/tags/categories.blade.php
+++ resources/views/tags/categories.blade.php
@@ -28,9 +28,35 @@
         <button onclick="location.href='{{route("tags.categories")}}'" class="text-white mx-1 px-3 py-1 border-0 rounded shadow" style="background-color:#ffc700;">
           <strong>タグは検索からでも見つかります</strong>
         </button>
+        
       </div>          
 
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9067426465896411"
+     crossorigin="anonymous"></script>
+      <!-- Horizontal -->
+      <ins class="adsbygoogle"
+          style="display:block"
+          data-ad-client="ca-pub-9067426465896411"
+          data-ad-slot="5078046569"
+          data-ad-format="auto"
+          data-full-width-responsive="true"></ins>
+      <script>
+          (adsbygoogle = window.adsbygoogle || []).push({});
+      </script>
+
+      
   </div>
   
-
+  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9067426465896411"
+  crossorigin="anonymous"></script>
+  <!-- Horizontal -->
+  <ins class="adsbygoogle"
+      style="display:block"
+      data-ad-client="ca-pub-9067426465896411"
+      data-ad-slot="5078046569"
+      data-ad-format="auto"
+      data-full-width-responsive="true"></ins>
+  <script>
+      (adsbygoogle = window.adsbygoogle || []).push({});
+  </script>
 @endsection
