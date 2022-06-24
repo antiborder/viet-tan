a6cc9a690ceb72e52844d183fca8390ea885e4bd
antiborder <mail@antiborder.com>
2022-06-24 08:00:53 +0700

    place ad in component

--- resources/views/words/learn.blade.php
+++ resources/views/words/learn.blade.php
@@ -19,25 +19,7 @@
         guest_level= "{{config('const.GUEST_LEVEL')}}"
       >
       </learn>
-
-
     </div>
-
   </div>
-
-  <div id="divadsensedisplaynone1" style=" ">
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9067426465896411"
-        crossorigin="anonymous"></script>
-    <!-- Horizontal -->
-    <ins class="adsbygoogle"
-        style="display:block; min-width:250px"
-        data-ad-client="ca-pub-9067426465896411"
-        data-ad-slot="5078046569"
-        data-ad-format="horizontal"
-        data-full-width-responsive="true"></ins>
-    <script>
-        (adsbygoogle = window.adsbygoogle || []).push({});
-    </script>                                  
-  <div>  
 
 @endsection
