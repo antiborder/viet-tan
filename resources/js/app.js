0fbd2077d38925ddde0f2fcd86fbf2db6b72bfc8
antiborder <mail@antiborder.com>
2022-06-24 07:35:05 +0700

    place ad in component

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,11 @@ import Vue from 'vue'
 import Measure from './components/Measure'
 import Learn from './components/Learn'
 
+//vue-google-adsense
+import Ads from 'vue-google-adsense'
+Vue.use(require('vue-script2'))
+Vue.use(Ads.Adsense)
+
 const app = new Vue({
   el: '#app',
   components: {
--- resources/views/words/learn.blade.php
+++ resources/views/words/learn.blade.php
@@ -39,19 +39,5 @@
         (adsbygoogle = window.adsbygoogle || []).push({});
     </script>                                  
   <div>  
-  <div id="divadsensedisplaynone2" style=" ">
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9067426465896411"
-        crossorigin="anonymous"></script>
-    <!-- Horizontal -->
-    <ins class="adsbygoogle"
-        style="display:block; min-width:250px"
-        data-ad-client="ca-pub-9067426465896411"
-        data-ad-slot="5078046569"
-        data-ad-format="horizontal"
-        data-full-width-responsive="true"></ins>
-    <script>
-        (adsbygoogle = window.adsbygoogle || []).push({});
-    </script>                                  
-  <div>  
 
 @endsection
