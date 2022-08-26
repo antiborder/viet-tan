487e14763e9d2eedc5adebb193e66bdca0e1f872
antiborder <mail@antiborder.com>
2022-08-26 16:14:53 +0700

    appearance of categories

--- resources/views/ads/rectangle.blade.php
+++ resources/views/ads/rectangle.blade.php
@@ -1,12 +1,12 @@
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9067426465896411"
-    crossorigin="anonymous"></script>
-<!-- square -->
-<ins class="adsbygoogle"
-    style="display:block"
-    data-ad-client="ca-pub-9067426465896411"
-    data-ad-slot="5284563145"
-    data-ad-format="rectangle"
-    data-full-width-responsive="true"></ins>
-<script>
-    (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9067426465896411"
+        crossorigin="anonymous"></script>
+    <!-- square -->
+    <ins class="adsbygoogle"
+        style="display:block"
+        data-ad-client="ca-pub-9067426465896411"
+        data-ad-slot="5284563145"
+        data-ad-format="rectangle"
+        data-full-width-responsive="true"></ins>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
